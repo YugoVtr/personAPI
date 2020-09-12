0e004acbc47b9db6d8ed9608c66d9a1af39c7bd5
yugovtr <vtrhg69@hotmail.com>
2020-09-12 00:02:37 -0300

    Initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Globaltec
+{
+  public class Person
+  {
+    public int Id { get; set; }
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    public string Uf { get; set; }
+    public DateTime Birthday { get; set; }
+  }
+}
